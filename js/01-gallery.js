a07e033e8a7df7845a552830d098364b5e42f116
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2023-09-29 18:40:48 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,33 +3,25 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 gallery.insertAdjacentHTML('afterbegin', creatMarkup(galleryItems));
 gallery.addEventListener('click', handlerOpen);
-window.addEventListener('keydown', handlerClose);
+
 let instance;
 function handlerOpen(evt) {
   evt.preventDefault();
 
-  const item = evt.target.alt;
-  const items = galleryItems.find(({ description }) => item === description);
-  console.log(items);
-  instance = basicLightbox.create(`
-  <div>
-    <img
-      class="gallery__image"
-      src="${items.original}"
-      data-source="${items.original}"
-      alt="${items.description}"
-    />
-  </div>
-`,
-)
-instance.show();
+  const item = evt.target;
+  if (item.nodeName !== 'IMG') {
+    return;
+  }
+  instance = basicLightbox.create(`<img src = "${item.dataset.source}"/>`)
+  instance.show();
+  window.addEventListener('keydown', handlerClose);
 };
 
 function handlerClose(evt) {
   if (evt.code === 'Escape') {
-     instance.close()
+    instance.close()
+    window.removeEventListener('keydown', handlerClose);
   }
-
 };
 
 function creatMarkup(arr) {
